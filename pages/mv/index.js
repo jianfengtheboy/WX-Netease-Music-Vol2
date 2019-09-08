59e8b455a15fc908c889250ba02967a87875fa67
jianfengtheboy <jianfengtheboy@163.com>
2019-09-08 11:44:33 +0800

    mv

--- pages/mv/index.js
+++ pages/mv/index.js
@@ -1,66 +1,100 @@
-// pages/mv/index.js
+var common = require('../../utils/util.js')
+var bsurl = require('../../utils/bsurl.js')
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    main: {},
+    tab: 0,
+    src: "",
+    rec: {},
+    loading: true,
+    offset: 0,
+    limit: 20,
+    recid: 0,
+    loading2: true,
+    simi: {},
+    id: ""
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    let that = this
+    wx.request({
+      url: bsurl + 'mv',
+      data: {
+        id : options.id
+      },
+      success: function(res) {
+        wx.getNetworkType({
+          complete: function(r) {
+            let src = res.data.data.brs
+            let wifi = r.networkType != 'wifi' ? false : true
+            let url = src[1080] || src[720] || src[480] || src[240]
+            url = bsurl + 'mv/url?url=' + url
+            that.setData({
+              id: options.id,
+              main: res.data.data,
+              src:url,
+              wifi: wifi,
+              loading:false,
+              recid: res.data.data.commentThreadId
+            })
+          } 
+        })
+        wx.setNavigationBarTitle({
+          title: res.data.data.name
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  tab: function(e) {
+    let t = e.currentTarget.dataset.tab
+    this.setData({
+      tab: t
+    })
+    let that = this
+    if (this.data.tab == 1 && this.data.rec.code != 200) {
+      common.loadrec(app.globalData.cookie, this.data.offset, this.data.limit, this.data.recid, function(data) {
+        that.setData({
+          loading: false,
+          rec: data,
+          offset: data.comments.length
+        })
+      }, 1)
+    }
+    if (this.data.tab == 2 && this.data.simi.code != 200) {
+      that.setData({
+        loading: true
+      })
+      wx.request({
+        url: bsurl + 'mv/simi',
+        data: {
+          id : that.data.id
+        },
+        success: function(res) {
+          that.setData({
+            loading: false,
+            simi: res.data
+          })
+        }
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  loadmore: function() {
+    if (this.data.rec.more && !this.data.loading) {
+      let that = this
+      this.setData({
+        loading2: true
+      })
+      common.loadrec(app.globalData.cookie, this.data.offset, this.data.limit, this.data.recid, function(data) {
+        let rec = that.data.rec
+        let offset = that.data.offset + data.comments.length
+        data.comments = rec.comments.concat(data.comments)
+        data.hotComments = rec.hotComments
+        that.setData({
+          loading2: false,
+          rec: data,
+          offset: offset
+        })
+      }, 1)
+    }
   }
 })
