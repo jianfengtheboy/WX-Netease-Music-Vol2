cc88c211bca9d011b3d88c456ad7e53307a7f020
jianfengtheboy <jianfengtheboy@163.com>
2019-09-10 23:41:48 +0800

    artist“

--- pages/artist/index.js
+++ pages/artist/index.js
@@ -1,4 +1,4 @@
-var bsurl = require('../../utils/async.js')
+var bsurl = require('../../utils/bsurl.js')
 var app = getApp()
 Page({
   data: {
@@ -44,7 +44,7 @@ Page({
       tab: t
     })
     let that = this
-    if (t === 2 && !this.data.album.loading) {
+    if (t == 2 && !this.data.album.loading) {
       this.setData({
         loading: false
       })
@@ -52,12 +52,69 @@ Page({
         url: bsurl + 'artist/album',
         data: {
           id: that.data.art.artist.id,
-          offset: that.data.album.o,
+          offset: that.data.album.offset,
           limit: 20
         },
         success: function(res) {
           res.data.loading = true
-          
+          res.data.offset = that.data.album.offset + res.data.hotAlbums.length
+					that.setData({
+						album: res.data,
+						loading: true
+					})
+        }
+      })
+    }
+    if (t == 3 && !this.data.mvs.loading) {
+      this.setData({
+        loading: false
+      })
+      wx.request({
+        url: bsurl + 'artist/mv',
+        data: {
+          id: that.data.art.artist.id,
+					offset: that.data.mvs.offset,
+					limit: 20
+        },
+        success: function(res) {
+					res.data.loading = true
+					res.data.offset = that.data.mvs.offset + res.data.mvs.length
+					that.setData({
+						mvs: res.data,
+						loading: true
+					})
+				}
+      })
+    }
+    if (t == 4 && !this.data.desc.loading) {
+      this.setData({
+        loading: false
+      })
+      wx.request({
+        url: bsurl + 'artist/desc',
+        data: {
+          id: that.data.art.artist.id
+        },
+        success: function(res) {
+          res.data.loading = true
+          that.setData({
+            loading: true,
+            desc: res.data
+          })
+        }
+      })
+      wx.request({
+        url: bsurl + 'artist/simi',
+        data: {
+          id: that.data.art.artist.id,
+					cookie: app.globalData.cookie
+        },
+        success: function(res) {
+          if(res.data.code != 200) return
+					res.data.loading = true
+					that.setData({
+					  simi: res.data
+					})
         }
       })
     }
