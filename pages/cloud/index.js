bbf8ec5db8a3891e182c40cbd66eb7d2d857660a
jianfengtheboy <jianfengtheboy@163.com>
2019-09-11 22:33:59 +0800

    cloud

--- pages/cloud/index.js
+++ pages/cloud/index.js
@@ -1,66 +1,92 @@
-// pages/cloud/index.js
+var appInstance = getApp()
+var bsurl = require('../../utils/bsurl.js')
+var id2Url = require('../../utils/base64md5.js')
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    list: {},
+    offset: 0,
+    limit: 20,
+    loading: false,
+    curplay: -1
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    this.getcloud()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getcloud: function(isadd) {
+    let that = this
+    wx.request({
+      url: bsurl + 'user/cloud',
+      data: {
+        offset: that.data.offset,
+        limit: that.data.limit
+      },
+      success: function(res) {
+        wx.stopPullDownRefresh()
+        if (res.data.code != 200) {
+          wx.showToast({
+            title: "云盘数据获取失败！",
+            duration: 2000
+          })
+          that.setData({
+            loading: true
+          })
+          return
+        }
+        if (isadd) {
+          that.data.offset += res.data.data.length
+          res.data.data = that.data.list.data.concat(res.data.data)
+        } else {
+          that.data.offset = res.data.data.length
+        }
+        let list = res.data
+        list.size = (res.data.size / 1073741824).toFixed(2)
+        list.maxSize = (res.data.maxSize / 1073741824)
+        list.percent = (list.size / list.maxSize).toFixed(2)
+        that.setData({
+          loading: true,
+          offset: that.data.offset,
+          list: list
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
-
+    this.data.offset = 0
+    this.getcloud()
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
-
+    this.data.list.hasMore && this.getcloud(1)
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  playall: function (event) {
+    this.setplaylist(this.data.canplay[0], 0)
+    appInstance.seekmusic(1)
+  },
+  setplaylist: function (music, index) {
+    //设置播放列表，设置当前播放音乐，设置当前音乐在列表中位置
+    appInstance.globalData.curplay = appInstance.globalData.curplay.id != music.id ? music : appInstance.globalData.curplay
+    appInstance.globalData.index_am = index //event.currentTarget.dataset.idx;
+    appInstance.globalData.playtype = 1
+    let shuffle = appInstance.globalData.shuffle
+    appInstance.globalData.list_sf = this.data.canplay //this.data.list.tracks;
+    appInstance.shuffleplay(shuffle)
+    appInstance.globalData.globalStop = false
+    this.setData({
+      curplay: music.id
+    })
+  },
+  playmusic: function (event) {
+    let that = this
+    let music = event.currentTarget.dataset.idx
+    let st = event.currentTarget.dataset.st
+    if (st * 1 < 0) {
+      wx.showToast({
+        title: '歌曲已下架',
+        icon: 'success',
+        duration: 2000
+      })
+      return
+    }
+    music = this.data.list.playlist.tracks[music]
+    that.setplaylist(music, event.currentTarget.dataset.idx)
   }
 })
