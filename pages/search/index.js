ec32acf7071c5f65820248cf9cdc34a632478b35
jianfengtheboy <jianfengtheboy@163.com>
2019-09-13 01:13:15 +0800

    搜索

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,199 @@
-// pahes/search/index.js
+var bsurl = require('../../utils/bsurl.js')
+var typelist = require('../../utils/searchtypelist.js')
+var nt = require('../../utils/nt.js')
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    tab: { 
+      tab: typelist[0].type, 
+      index: 0 
+    },
+    value: "",
+    tabs: typelist,
+    recent: wx.getStorageSync("recent") || [],
+    loading: false,
+    prevalue: ""
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    let v = options.key
+    v && this.search(v)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  inputext: function (e) {
+    let name = e.detail.value
+    this.setData({ 
+      value: name
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  playmusic: function (event) {
+    let that = this
+    let music = event.currentTarget.dataset.idx
+    let st = event.currentTarget.dataset.st
+    if (st * 1 < 0) {
+      wx.showToast({
+        title: '歌曲已下架',
+        icon: 'success',
+        duration: 2000
+      })
+      return
+    }
+    music = that.data.tabs[0].relist.songs[music]
+    app.globalData.curplay = music
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  search: function (name) {
+    if (!name || (name == this.data.prevalue)) return
+    let index = this.data.tab.index
+    let tl = typelist
+    this.setData({
+      tabs: tl,
+      prevalue: name,
+      value: name,
+      loading: true
+    })
+    let curtab = this.data.tabs[index]
+    let that = this
+    tl = this.data.tabs
+    this.httpsearch(name, curtab.offset, this.data.tab.tab, function (res) {
+      curtab.relist = res
+      curtab.loading = true
+      let resultarry = res.songs || res.artists || res.albums || res.playlists || res.mvs || res.djprograms || res.userprofiles || []
+      curtab.offset = resultarry.length
+      let size = res.songCount || res.artistCount || res.albumCount || res.playlistCount || res.mvCount || res.djprogramCount || res.userprofileCount
+      size = size ? size : 0
+      curtab.none = curtab.offset >= size ? true : false
+      tl[index] = curtab
+      let recent = that.data.recent
+      let curname = recent.findIndex(function (e) { return e == name })
+      if (curname > -1) {
+        recent.splice(curname, 1)
+      }
+      recent.unshift(name)
+      wx.setStorageSync('recent', recent)
+      that.setData({
+        tabs: tl,
+        loading: true,
+        recent: recent,
+        prevalue: name
+      })
+    }, function () {
+      curtab.loading = true
+      curtab.none = true
+      tl[index] = curtab
+      that.setData({
+        tabs: tl
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  searhFrecent: function (e) {
+    this.search(e.currentTarget.dataset.value)
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  searhFinput: function (e) {
+    this.search(e.detail.value.name)
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  onReachBottom: function (e) {
+    let tl = this.data.tabs
+    let that = this
+    let curtab = tl[this.data.tab.index]
+    if (curtab.none) return
+    curtab.loading = false
+    tl[this.data.tab.index] = curtab
+    this.setData({
+      tabs: tl
+    })
+    this.httpsearch(this.data.prevalue, curtab.offset, this.data.tab.tab, function (res) {
+      curtab.loading = true
+      let resultarry = res.songs || res.artists || res.albums || res.playlists || res.mvs || res.djprograms || res.userprofiles || []
+      let size = res.songCount || res.artistCount || res.albumCount || res.playlistCount || res.mvCount || res.djprogramCount || res.userprofileCount
+      size = size ? size : 0
+      let length = resultarry.length
+      curtab.offset = curtab.offset + length
+      curtab.none = curtab.offset >= size ? true : false
+      curtab.relist.songs = curtab.relist.songs ? curtab.relist.songs.concat(resultarry) : null
+      curtab.relist.artists = curtab.relist.artists ? curtab.relist.artists.concat(resultarry) : null
+      curtab.relist.albums = curtab.relist.albums ? curtab.relist.albums.concat(resultarry) : null
+      curtab.relist.playlists = curtab.relist.playlists ? curtab.relist.playlists.concat(resultarry) : null
+      curtab.relist.mvs = curtab.relist.mvs ? curtab.relist.mvs.concat(resultarry) : null
+      curtab.relist.djprograms = curtab.relist.djprograms ? curtab.relist.djprograms.concat(resultarry) : null
+      curtab.relist.userprofiles = curtab.relist.userprofiles ? curtab.relist.userprofiles.concat(resultarry) : null
+      tl[that.data.tab.index] = curtab
+      that.setData({
+        tabs: tl
+      })
+    }, function () {
+      curtab.loading = true
+      curtab.none = true
+      tl[that.data.tab.index] = curtab
+      that.setData({
+        tabs: tl
+      })
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  httpsearch: function (name, offset, type, cb, err) {
+    wx.request({
+      url: bsurl + 'search',
+      data: {
+        keywords: name,
+        offset: offset,
+        limit: 20,
+        type: type
+      },
+      method: 'GET',
+      success: function (res) {
+        cb && cb(res.data.result)
+      },
+      fail: function () {
+        err && err()
+      }
+    })
+  },
+  tabtype: function (e) {
+    let index = e.currentTarget.dataset.index
+    let curtab = this.data.tabs[index]
+    let type = e.currentTarget.dataset.tab
+    let that = this
+    let tl = that.data.tabs
+    if (!curtab.loading) {
+      this.httpsearch(this.data.prevalue, curtab.offset, type, function (res) {
+        curtab.relist = res
+        curtab.loading = true
+        let resultarry = res.songs || res.artists || res.albums || res.playlists || res.mvs || res.djprograms || res.userprofiles || []
+        curtab.offset = resultarry.length
+        let size = res.songCount || res.artistCount || res.albumCount || res.playlistCount || res.mvCount || res.djprogramCount || res.userprofileCount
+        size = size ? size : 0
+        curtab.none = curtab.offset >= size ? true : false
+        tl[index] = curtab
+        that.setData({
+          tabs: tl
+        })
+      }, function () {
+        curtab.loading = true
+        curtab.none = true
+        tl[index] = curtab
+        that.setData({
+          tabs: tl
+        })
+      })
+    }
+    this.setData({
+      tab: {
+        tab: type,
+        index: index
+      }
+    })
+  },
+  clear_kw: function () {
+    this.setData({
+      value: "",
+      loading: false,
+      tabs: typelist,
+      prevalue: ""
+    })
+  },
+  del_research: function (e) {
+    let index = e.currentTarget.dataset.index
+    this.data.recent.splice(index, 1)
+    this.setData({
+      recent: this.data.recent
+    })
+    wx.setStorageSync('recent', this.data.recent)
   }
 })
